b7157496c8bd9e99ffb4f9fc0321251333d43379
sak39 <dnsn.isk@gmail.com>
2017-04-30 05:09:04 +0900

    add mkbrewfile && set it into launchd

--- init.sh
+++ init.sh
@@ -6,15 +6,14 @@ exists() {
     command -v "$1" > /dev/null 2>&1
 }
 ask() {
-  printf "$* [Y/n] "
+  printf "$* [y/N] "
   local answer
   read answer
 
-  #todo: Yesをデフォルトにしたいな
   case $answer in
-    "no" )  return 1 ;;
-    "n"  )  return 1 ;;
-    * )     return 0 ;;
+    "yes" )  return 0 ;;
+    "y"   )  return 0 ;;
+    * )     return 1 ;;
   esac
 }
 
@@ -35,10 +34,7 @@ do
     # 無視したいファイルやディレクトリはこんな風に追加してね
     #    [[ ${f} = ".git" ]] && continue
     #    [[ ${f} = ".gitignore" ]] && continue
-    [[ ${f} = "configs" ]] && continue
 
-    #シンボリックリンクでdotfilesを管理すれば管理がシンプルになるね
-    ### todo: ここでoverwriteの条件分岐を起こす必要があるかもしれない
     ln -snfv ${SYMLINK_DIR}/${f} ${HOME}/${f}
 done
 cd ${DOT_DIR}
@@ -63,15 +59,33 @@ echo $(tput setaf 2)"Configuration complete. ✔"$(tput sgr0)
 ## 以降の処理にはzshのインストールが先 ##
 if ask "set default shell to '/usr/local/bin/zsh' ?"; then
     echo $(tput setaf 2)"START: chsh -s /usr/local/bin/zsh"$(tput sgr0)
-    if [ ! -f "/usr/local/bin/zsh" ]; then
+    BREW_ZSH_LOCATION=$(which zsh)
+    echo $(tput setaf 1)"DEBUG: ${BREW_ZSH_LOCATION}"$(tput sgr0) #@@
+    if [ ${BREW_ZSH_LOCATION} != "/usr/local/bin/zsh" ]; then
         echo $(tput setaf 4)"ERROR: brew-zsh does not installed!!"$(tput sgr0)
         exit 1
     fi
-    sudo echo "/usr/local/bin/zsh" >> /etc/shells
-    chsh -s $(which zsh)
-    echo $(tput setaf 2)"Change shell complete. ✔"$(tput sgr0)
+#    if [ ! -f "/usr/local/bin/zsh" ]; then
+#        echo $(tput setaf 4)"ERROR: brew-zsh does not installed!!"$(tput sgr0)
+#        exit 1
+#    fi
+
+    sudo sh -c "echo ${BREW_ZSH_LOCATION} >> /etc/shells"
+    # If it writes into /etc/shells successfully
+    if [ $? -eq "0" ]; then
+        chsh -s ${BREW_ZSH_LOCATION}
+        echo $(tput setaf 2)"Change shell complete. ✔"$(tput sgr0)
+        echo $(tput setaf 2)"But.. 'chsh: no changes made' appears in there. In this case, you can change your shell at SystemPreference/User&Groups/AdvancedOptions."$(tput sgr0)
+    else
+        echo $(tput setaf 6)"WARNING: Failed writing into /etc/shells"$(tput sgr0)
+    fi
 fi
 
+# normal-zsh are installed?
+if  ! exists zsh ; then
+    echo $(tput setaf 4)"ERROR: 'zsh' doesn't installed!!"$(tput sgr0)
+    exit 1
+fi
 
 # Install prezto
 if [[ ! -d ${HOME}/.zprezto ]]; then
@@ -85,7 +99,7 @@ fi
 # Install zplug
 if [[ ! -d ${HOME}/.zplug ]]; then
     echo $(tput setaf 2)"START: Install 'zplug'"$(tput sgr0)
-    curl -sL zplug.sh/installer | zsh  #this way is better than brew
+    curl -sL --proto-redir -all,https https://zplug.sh/installer | zsh
     echo $(tput setaf 2)"'zplug' installation complete. ✔"$(tput sgr0)
 else
     echo $(tput setaf 6)"WARNING: 'zplug' is already installed."$(tput sgr0)
@@ -93,14 +107,13 @@ fi
 
 # SSH key
 if ask "Do you want to create ssh key pair?"; then
+    # todo: .ssh directoryがない状態でのssh-keygenがどのような挙動をするのかを調べる
     echo $(tput setaf 2)"START: make ssh key pair"$(tput sgr0)
-    if [[ ! -d ${HOME}/.ssh ]]; then
-        if exists "ssh-keygen"; then
-            ssh-keygen -t rsa -C $(whoami) #comment: USER-NAME
-            echo $(tput setaf 2)"make ssh key pair complete. ✔"$(tput sgr0)
-        else
-            echo $(tput setaf 6)"WARNING: 'ssh-keygen' does not installed."$(tput sgr0)
-        fi
+    if exists "ssh-keygen"; then
+        ssh-keygen -t rsa -C $(whoami) #comment: USER-NAME
+        echo $(tput setaf 2)"make ssh key pair complete. ✔"$(tput sgr0)
+    else
+        echo $(tput setaf 6)"WARNING: 'ssh-keygen' does not installed."$(tput sgr0)
     fi
 fi
 
--- test.sh
+++ test.sh
@@ -1,21 +1,35 @@
 # functions
+echo <<- 'EOF'
+      $(tput setaf 4)
+       ERROR: Failed writing into /etc/shells.
+       Please change your shell from 'System Preference/User&Groups/AdvancedOptions(RightClick on your User Icon)
+       $(tput sgr0)
+EOF
+
+BREW_ZSH_LOCATION=$(which zsh)
+echo ${BREW_ZSH_LOCATION}
+if [ ${BREW_ZSH_LOCATION} != "/usr/local/bin/zsh" ]; then
+    echo $(tput setaf 4)"ERROR: brew-zsh does not installed!!"$(tput sgr0)
+    exit 1
+fi
+
 exists() {
     command -v "$1" > /dev/null 2>&1
 }
 ask() {
-  printf "$* [Y/n] "
+  printf "$* [y/N] "
   local answer
   read answer
 
   #todo: Yesをデフォルトにしたいな
   case $answer in
-    "no" )  return 1 ;;
-    "n"  )  return 1 ;;
-    * )     return 0 ;;
+    "yes" )  return 0 ;;
+    "y"   )  return 0 ;;
+    * )     return 1 ;;
   esac
 }
 
-if exists "docker"; then
+if ask "hoge"; then
     echo "1"
 else
     echo "0"
