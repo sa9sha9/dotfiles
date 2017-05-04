0d1657f93d93562bff4ac154437fd199ca92dc2f
sa9sha9 <sa9sha9@gmail.com>
2017-05-05 01:30:54 +0900

    add Application Support, Preferences

---
+++ configs/Preferences/IntelliJIdea2017.1/consoles/.history/db/8f8b9550-81b9-4f57-98dc-d3ce67701123.sql
@@ -0,0 +1,5 @@
+select * from wp_options
+where option_name like '%media%'
+;-- -. . -..- - / . -. - .-. -.--
+select * from wp_options
+where option_name like '%test%'
---
+++ configs/Preferences/IntelliJIdea2017.1/consoles/db/8f8b9550-81b9-4f57-98dc-d3ce67701123/console.sql
@@ -0,0 +1,2 @@
+select * from wp_options
+where option_name like '%test%'
