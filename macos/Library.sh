540e30fc2702f0bc01d3ba493c8fb3ebd0e895cf
sa9sha9 <sa9sha9@gmail.com>
2018-02-02 08:25:54 +0900

    Add new shell script for setting up Home Libraries

---
+++ macos/Library.sh
@@ -0,0 +1,34 @@
+SYNC_DIR=${HOME}/Sync/GoogleDrive/Sync/macOS
+LIBRARY_DIR=${HOME}/Library
+
+if [[ ! -d ${SYNC_DIR} ]]; then
+    echo "ERROR: $SYNC_DIR not exists."
+    exit 1
+fi
+
+# home setting file
+ln -snfv ${SYNC_DIR}/.config ${HOME}
+ln -snfv ${SYNC_DIR}/.ssh ${HOME}
+
+# iTerm2
+ln -snfv ${SYNC_DIR}/iTerm2 ${LIBRARY_DIR}
+ln -snfv ${SYNC_DIR}/iTerm2/sak.iterm2.config $HOME/Library/Application\ Support/iTerm2/DynamicProfiles
+
+# Dash
+ln -snfFv ${SYNC_DIR}/Dash ${LIBRARY_DIR}
+
+
+# plist
+for f in ${SYNC_DIR}/Library/Preferences/*; do
+    # todo: ignore 'Icon?'
+    #        [[ ${f} = "Icon" ]] && continue
+    ln -snfFv ${f} ${LIBRARY_DIR}/Preferences
+done
+# 先頭 "." を処理できてないので別個で
+ln -snfv ${SYNC_DIR}/Library/Preferences/.GlobalPreferences.plist ${LIBRARY_DIR}/Preferences
+
+
+# printer
+for f in ${SYNC_DIR}/Library/Printers/*; do
+    ln -snfFv ${f} ${LIBRARY_DIR}/Printers
+done
