74c4b88b255ca827d4b06f3a960426f2a2d0a1b7
sa9sha9 <sa9sha9@gmail.com>
2018-02-11 16:51:18 +0900

    Searete util functions to use from all

--- entry.sh
+++ entry.sh
@@ -1,40 +1,8 @@
 #!/usr/bin/env bash
 
-# args
-DOTFILES_DIR=${PWD} # => ~/git/dotfiles
-
-# functions for shell-script
-exists() {
-  command -v "$1" >/dev/null 2>&1
-}
-ask() {
-  printf "$* [y/N] "
-  local answer
-  read answer
-
-  case $answer in
-    "yes" )  return 0 ;;
-    "y"   )  return 0 ;;
-    * )     return 1 ;;
-  esac
-}
-
-# usage when exit
-usage_exit() {
-        echo "Usage: $0 [-e (deploy/init)]" 1>&2
-        exit 1
-}
-
-# Check arguments and Get the first argument
-get_opt() {
-    if [ $# -eq 1 ] ; then
-        echo $1 #これが戻り値になる
-        return 0
-    else
-        usage_exit
-	fi
-}
-
+# prepare util
+DOTFILES_DIR=${HOME}/git/dotfiles
+source ${DOTFILES_DIR}/util.sh
 
 export DOTFILES_DIR
 export SYMLINK_DIR=${DOTFILES_DIR}/symlink
---
+++ util.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# functions for shell-script
+exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+ask() {
+  printf "$* [y/N] "
+  local answer
+  read answer
+
+  case $answer in
+    "yes" )  return 0 ;;
+    "y"   )  return 0 ;;
+    * )     return 1 ;;
+  esac
+}
+
+# usage when exit
+usage_exit() {
+        echo "Usage: $0 [-e (deploy/init)]" 1>&2
+        exit 1
+}
+
+# Check arguments and Get the first argument
+get_opt() {
+    if [ $# -eq 1 ] ; then
+        echo $1 #これが戻り値になる
+        return 0
+    else
+        usage_exit
+	fi
+}
+
+export -f exists
+export -f ask
+export -f usage_exit
+export -f get_opt
